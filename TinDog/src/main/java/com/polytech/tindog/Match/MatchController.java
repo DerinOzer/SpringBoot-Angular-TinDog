356be4bd22c6c503bf2c8ac48a1e09b7a6fe299b
Derin.ozer <derin.ozer@intertech.com.tr>
2023-02-02 18:53:39 +0100

    Matches completed.

--- TinDog/src/main/java/com/polytech/tindog/Match/MatchController.java
+++ TinDog/src/main/java/com/polytech/tindog/Match/MatchController.java
@@ -1,7 +1,31 @@
 package com.polytech.tindog.Match;
 
+import com.polytech.tindog.Dog.Dog;
+import com.polytech.tindog.Owner.Owner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class MatchController {
+
+    @Autowired
+    MatchService matchService;
+
+    @PostMapping("/create-match")
+    public void createOwner(@RequestParam String judgingId, @RequestParam String judgedId, @RequestParam Boolean liked) throws Exception {
+        matchService.createMatch(judgingId,judgedId,liked);
+    }
+
+    @GetMapping("/get-matches-of-owner")
+    public List<Owner> getMatchesOfOwnerById(@RequestParam String ownerId) throws Exception{
+        return matchService.getMatchesOfOwner(ownerId);
+    }
 }
